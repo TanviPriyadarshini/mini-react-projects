7c515fb9fb8b8f4f71dfc58695dd730e5405f273
TanviPriyadarshini <tanvipriyadarshini@gmail.com>
2024-08-29 18:34:41 +0700

    added more features to the stepper component

--- stepper/src/components/CheckoutSteps.jsx
+++ stepper/src/components/CheckoutSteps.jsx
@@ -1,38 +1,58 @@
-import React, {useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import { FaCheck } from "react-icons/fa6";
 
 const CheckoutSteps = ({stepsConfig = []}) => {
     const [activeStep, setactiveStep] = useState(1);
-    const [isComplete, setIsComplete] = useState(false);
+    const [margins, setMargins] = useState({
+        marginLeft: 0,
+        marginRight: 0
+    });
 
+    const steps = useRef([])
     const ActiveComponent = stepsConfig[activeStep-1].Component
+
+    useEffect(() => {
+        setMargins({
+            marginLeft: steps.current[0].offsetWidth,
+            marginRight: (steps.current[stepsConfig.length - 1].offsetWidth)
+        })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[steps])
+
+    const calculateWidth = () => {
+        return ((activeStep-1)/(stepsConfig.length-1)*100)
+    }
 
     const handleNextClick = () => {
         setactiveStep(prev => prev + 1)
     }
   
   return (
-    <>
+    <div className="relative">
         <div className="flex w-[100%] justify-around">
-        {stepsConfig && stepsConfig.map((step, index) => {
+            {stepsConfig && stepsConfig.map((step, index) => {
             
             return (
-                <div key={index} >
+                <div 
+                    key={index}
+                    ref={el => steps.current[index] = el} // ref to the div created
+                >
+
                     <div className="flex flex-col justify-center items-center">
                         {
                             activeStep === (index +1) ? 
                             <div 
-                            className=" rounded-full w-[30px] h-[30px] flex justify-center items-center bg-blue-600"
+                            className=" rounded-full w-[30px] h-[30px] flex justify-center items-center bg-blue-600 z-10"
                             >
                                 {index + 1}
                             </div> : 
-                                isComplete ?  
+                                activeStep > (index + 1) ?  
                                     <div 
-                                    className="rounded-full w-[30px] h-[30px] flex justify-center items-center bg-green-600">
+                                    className="rounded-full w-[30px] h-[30px] flex justify-center items-center bg-green-600 z-10">
                                        <FaCheck/>
                                     </div> : 
                                     <div 
-                                    className="bg-[#ccc] rounded-full w-[30px] h-[30px] flex justify-center items-center"
+                                    className="bg-[#ccc] rounded-full w-[30px] h-[30px] flex justify-center items-center z-10"
                                     >
                                         {index + 1}
                                     </div>
@@ -44,11 +64,17 @@ const CheckoutSteps = ({stepsConfig = []}) => {
             );
         })}
         </div>
+        <div 
+        className="absolute top-[15%] left-0 h-1 bg-[#ccc]"
+        style={{width: `calc(100% - ${margins.marginLeft + margins.marginRight}px)`, marginLeft: margins.marginLeft, marginRight: margins.marginRight}}>
+            <div className="bg-[#28a745] h-[100%] transition" style={{transition: `0.2s ease`, width: `${calculateWidth()}%`}}></div>
+        </div>  
         <ActiveComponent/>
-        {activeStep <= stepsConfig.length && <button 
+        {activeStep < stepsConfig.length && 
+        <button 
         className="p-2 border rounded"
         onClick={handleNextClick}>Next</button>}
-    </>
+    </div>
   )
 }
 
